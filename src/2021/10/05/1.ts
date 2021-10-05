acc557888cb38c1a2bb8f4a10fb2eed4b5070b20
尛豆芽 <yejunkuan@163.com>
2021-10-05 15:15:21 +0800

    study

---
+++ src/2021/10/05/1.ts
@@ -0,0 +1,24 @@
+/**
+ * @description 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+function removeDuplicates(nums: number[]): number {
+  let j = 0
+  for(let i = 1; i< nums.length; i++) {
+    if(nums[j] !== nums[i]) {
+      j++
+      nums[j] = nums[i]
+    }
+  }
+  return j+1
+}
+
+console.log(removeDuplicates([1,2,3,4,1,2,1]))
+
+
